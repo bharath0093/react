8421dba90aee1281bff735c4e664e412f51e60b8
Bharath <br4925432@gmail.com>
2026-09-21 02:15:20 -0700

    course.js

---
+++ app/course.js
@@ -0,0 +1,18 @@
+import './Home.css';
+import React from 'react';
+function Course(){
+	return (
+		<React.Fragment>
+	<center>
+		<h1>THE COURSES OFFERED ARE</h1><br />
+		
+		<h2>CSE</h2>
+		<h2>E&C</h2>
+		<h2>CE</h2>
+		<h2>ME</h2>
+		<h2>MTT</h2>		
+		</center>
+	</React.Fragment>
+	)
+}
+export default Course;

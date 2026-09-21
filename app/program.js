913752bc30d37953244d3566b18ee0c48246364a
Bharath <br4925432@gmail.com>
2026-09-21 02:22:23 -0700

    program.js

---
+++ app/program.js
@@ -0,0 +1,17 @@
+import './Home.css';
+import React from 'react';
+function Program(){
+	return (
+		<React.Fragment>
+	<center>
+		<h1>THE PROGRAMS CONDUCTED ARE</h1><br />
+		
+		<h2>NSS</h2>
+		<h2>SPORTS</h2>
+		<h2>RED CROSS</h2>
+				
+		</center>
+	</React.Fragment>
+	)
+}
+export default Program;

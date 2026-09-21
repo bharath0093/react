20c1dcdde5638dc896a1f30653452695898a1bc8
Bharath <br4925432@gmail.com>
2026-09-21 02:16:21 -0700

    Home.js

---
+++ app/home.js
@@ -0,0 +1,28 @@
+import './Home.css';
+import React from 'react';
+function Home()  {
+	return(
+		<React.Fragment>
+		<center>
+		<h1>HOME PAGE</h1>
+		<p >
+		<b><center>VISION</center></b><br />
+
+		To Become one of the best institution by improving technical education and  make students to serve the industry,society at large.<br />
+
+		 
+
+		<center><b>MISSION</b></center><br />
+
+		1.Imparting technical education by providing good Teaching-Learning environment and  giving Self-Learning activities to students in the area of technological advancement to meet the requirement  of the industry.<br />
+
+		2.Incalculating Ethical and Leadership qualities by conducting variuos workshops.<br />
+
+		3.Encourage students take up Community-based activities through Naional Service Scheme and Training rural youth for self employment by conducting various Community Development activities.
+		</p>
+		</center>
+		</React.Fragment>
+			);
+};
+
+export default Home;
